c23fc8270774afe69bdd2b79c1545b1684987b85
quigongian <gipitz@protonmail.com>
2023-04-07 15:44:55 -0400

    Added home view controller and table view cell

---
+++ RizzTracker/HomeControllers/HomeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HomeViewController.swift
+//  RizzTracker
+//
+//  Created by User on 4/7/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return #number of users api call
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell with identifier, "TrackCell"
+        // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserTableViewCell
+//
+//        // Get the track that corresponds to the table view row
+//        let track = tracks[indexPath.row]
+//
+//        // Configure the cell with it's associated track
+//        cell.configure(with: track)
+//
+//        // return the cell for display in the table view
+//        return cell
+
+        <#code#>
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
