a97025c4d063b66ea930ccf453ed5f0819a69c9b
Nicholas de Souza <nrdesouzag13@gmail.com>
2023-04-06 22:35:42 -0400

    Separated User Struct into two to utilize ParseSwift

--- RizzTracker/AppDelegate.swift
+++ RizzTracker/AppDelegate.swift
@@ -12,7 +12,6 @@ import ParseSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) async -> Bool {
         // Override point for customization after application launch.
         
