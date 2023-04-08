a8776a1b5783c5889d48f16bf4c44a8c9cad07db
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2023-04-08 19:04:58 -0400

    added new files, adjusted attributes on the storyboard

---
+++ RizzTracker/Models/Math.swift
@@ -0,0 +1,31 @@
+//
+//  Math.swift
+//  RizzTracker
+//
+//  Created by Vincent Carrancho on 4/8/23.
+//
+
+import Foundation
+
+struct Math {
+    
+    static func calculateInitialRizz(selfRizz: Float, selfPerception: Float) -> Float {
+        return 0.2 + 0.06 * ((selfRizz/2) + (selfPerception/2))
+    }
+    
+    // TODO: Refine this formula
+    static func calculateRizz(quantities: [Int], weights: [Float], previousRizz: Int) -> Float {
+        if quantities.count != weights.count {
+            return -1
+        }
+        
+        var deltaRizz: Float = 0.0
+        for i in 0..<quantities.count {
+            deltaRizz += weights[i] * Float(quantities[i])
+        }
+        
+        deltaRizz = 10 + log10(abs(deltaRizz - 10))
+        
+        return Float(previousRizz) + deltaRizz
+    }
+}
