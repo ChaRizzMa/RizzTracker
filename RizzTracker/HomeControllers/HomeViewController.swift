a9e104ad25a237fa246d3e102d065b6845535fb1
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2023-04-09 22:56:41 -0400

    please test code

--- RizzTracker/HomeControllers/HomeViewController.swift
+++ RizzTracker/HomeControllers/HomeViewController.swift
@@ -6,42 +6,48 @@
 //
 
 import UIKit
-
+import ParseSwift
 
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    let query = Rizzults.query()
+    var rizzults: [Rizzults]? = [Rizzults]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
+        
+        
+        Task {
+            do {
+                // todo add fetch loading
+                print("Query starting ✅")
+                rizzults = try? await query.findAll()
+                print("Successfully Fetched ✅")
+                print(rizzults)
+                tableView.dataSource = self
+            } catch let error {
+                print("Error: ❌", error)
+            }
+        }
+        
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 extension HomeViewController: UITableViewDelegate {}
+
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return rizzults?.count ?? 10;
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! UserTableViewCell
-        cell.overallNameLabel.text = "99"
-        cell.userNameLabel.text = "Giancarlo"
+        var index = indexPath.item
+        cell.overallNameLabel.text = String(Int.random(in: 45...93))
+        cell.userNameLabel.text = String(rizzults![index].owner ?? "")
         return cell
     }
 }
