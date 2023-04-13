663840f8aaf56bda4b3ea2b6c9fdd9508763cfa6
SayfTanbir <108767812+SayfTanbir@users.noreply.github.com>
2023-04-12 21:33:59 -0400

    Moving over from the outdated version

--- RizzTracker/Rizz/MyRizzCellViewController.swift
+++ RizzTracker/Rizz/MyRizzCellViewController.swift
@@ -9,8 +9,78 @@ import UIKit
 
 class MyRizzCellViewController: UIViewController {
     
+    @IBOutlet weak var lblIntro: UILabel!
+    @IBOutlet weak var lblOVR: UILabel!
+    @IBOutlet weak var lblSheBad: UILabel!
+    @IBOutlet weak var lblSheWant: UILabel!
+    @IBOutlet weak var lblToTalk: UILabel!
+    @IBOutlet weak var lblWomanTalked: UILabel!
+    @IBOutlet weak var lblNumGot: UILabel!
+    
+    @IBOutlet weak var lblProDamnBad: UILabel!
+    @IBOutlet weak var lblProSheWant: UILabel!
+    @IBOutlet weak var lblProToTalk: UILabel!
+    @IBOutlet weak var lblProTalkedTo: UILabel!
+    
+    @IBOutlet weak var lblSubmittedRizz: UILabel!
+    @IBOutlet weak var lblInitialRizz: UILabel!
+    
+    @IBOutlet weak var lblEmail: UILabel!
+    @IBOutlet weak var lblPhoneNum: UILabel!
+    @IBOutlet weak var lblPronouns: UILabel!
+    @IBOutlet weak var lblPref: UILabel!
+    
+    var user: PFUser!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Task {
+            do {
+                user = try await PFUser.current()
+                print(user!)
+                var fName = user!.firstName
+                var lName = user!.lastName
+                lblIntro.text = String("Hello \(fName ?? "Default Value") \(lName ?? "Default Value")! Here are Your All Time Rizz Stats:")
+                //lblOVR.text =
+                var pronoun1 = ""
+                var pronoun2 = ""
+                var pronoun3 = ""
+                if user?.attractionPreference == "He/Him" {
+                    pronoun1 = "He"
+                    pronoun2 = "Him"
+                    pronoun3 = "Men"
+                } else if user?.attractionPreference == "She/Hers" {
+                    pronoun1 = "She"
+                    pronoun2 = "Her"
+                    pronoun3 = "Women"
+                } else {
+                    pronoun1 = "They"
+                    pronoun2 = "Them"
+                    pronoun3 = "People"
+                }
+                //lblSheBad.text =
+                lblProDamnBad.text = "Damn \(pronoun1) Bad's"
+                //lblSheWant.text =
+                lblProSheWant.text = "\(pronoun1) Want me fr fr's"
+                //lblToTalk.text =
+                lblProToTalk.text = "Going to talk to \(pronoun2)'s"
+                //lblWomanTalked.text =
+                lblProTalkedTo.text = "\(pronoun3) talked to"
+                //lblNumGot.text =
+                
+                //lblSubmittedRizz.text =
+                var initialRizz = NSString(format: "%.0f", user!.initialRizz ?? "")
+                lblInitialRizz.text = String(initialRizz)
+                
+                lblEmail.text = user!.email ?? ""
+                lblPhoneNum.text = user!.phoneNumber ?? ""
+                lblPronouns.text = user!.pronouns ?? ""
+                lblPref.text = user!.attractionPreference ?? ""
+            } catch let error {
+                print("an error occurred: \(error)")
+            }
+        }
         
         // Do any additional setup after loading the view.
     }
