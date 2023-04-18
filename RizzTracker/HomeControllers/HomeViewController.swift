d0d2c60e977fead2b6fe1360f6134d0334aae003
tehniel <skydan.inc@gmail.com>
2023-04-18 02:34:03 -0400

    refactor HomeView

--- RizzTracker/HomeControllers/HomeViewController.swift
+++ RizzTracker/HomeControllers/HomeViewController.swift
@@ -8,194 +8,12 @@
 import UIKit
 import ParseSwift
 
-// TODO: Fix teh damn index shit
-
-
-
 class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     let query = Rizzults.query()
-    var rizzults: [Rizzults] = [
-        Rizzults(
-        owner: "None",
-        objectID: "None",
-        badsQuantity: -1,
-        wantMeFrFRQuantity: -1,
-        goingToTalkToQuantity: -1,
-        numberComunications: -1,
-        descriptionOfSituation: "None"
-    ),
-        Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        ),Rizzults(
-            owner: "None",
-            objectID: "None",
-            badsQuantity: -1,
-            wantMeFrFRQuantity: -1,
-            goingToTalkToQuantity: -1,
-            numberComunications: -1,
-            descriptionOfSituation: "None"
-        )
-    ]
+    var rizzults: [Rizzults] = []
+    
     var overalls: [String] = []
     
     override func viewDidLoad() {
@@ -214,10 +32,8 @@ class HomeViewController: UIViewController {
                     
                     // TODO: Sort the data by date created.
                     self.rizzults = data
-                    print(self.rizzults)
                     self.rizzults = self.rizzults.sorted(by: { $0.updatedAt ?? Date.distantPast > $1.updatedAt ?? Date.distantPast })
                     
-                    //print(self?.Movies)
                     // Make the table view reload now that we have new data
                     self.tableView.reloadData()
                 }
@@ -238,26 +54,25 @@ extension HomeViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Bruh")
         performSegue(withIdentifier: "DetailViewSegue", sender: (Any).self)
-        print("Selected Row at \(indexPath.item)")
-        
     }
-    
 }
 
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rizzults.count ?? 40
+        return rizzults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! UserTableViewCell
         var index = indexPath.item
+        
         cell.ownerOverall = rizzults[index].ownerRizz ?? -1
         cell.overallNameLabel.text = String(rizzults[index].ownerRizz ?? -1) + " OVR"
         cell.userNameLabel.text = String( rizzults[index].owner ?? String(-1))
         let ownerOverall = rizzults[index].ownerRizz ?? -1
+        
         if case -1...50 = ownerOverall {
             cell.overallNameLabel.textColor = UIColor.red
             cell.overallNameLabel.font = UIFont.boldSystemFont(ofSize: 17)
@@ -271,7 +86,6 @@ extension HomeViewController: UITableViewDataSource {
             cell.overallNameLabel.textColor = UIColor(red: 0/255, green: 204/255, blue: 0/255, alpha: 1)
             cell.overallNameLabel.font = UIFont.boldSystemFont(ofSize: 17)
         }
-        
         return cell
     }
 }
