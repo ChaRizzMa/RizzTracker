d53e9db681d79d9b1aa1203d0e0fd29d765f7d9c
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2023-04-18 18:49:54 -0400

    fixed pronoun not working

--- RizzTracker/DetailControllers/DetailViewController.swift
+++ RizzTracker/DetailControllers/DetailViewController.swift
@@ -24,60 +24,71 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var pronoun1 = ""
-        var pronoun2 = ""
-        var pronoun3 = ""
-        if user?.attractionPreference == "He/Him" {
-            pronoun1 = "He"
-            pronoun2 = "Him"
-            pronoun3 = "Men"
-        } else if user?.attractionPreference == "She/Hers" {
-            pronoun1 = "She"
-            pronoun2 = "Her"
-            pronoun3 = "Women"
-        } else {
-            pronoun1 = "They"
-            pronoun2 = "Them"
-            pronoun3 = "People"
-        }
+        
+        
+        Task {
+            do {
+                user = try await PFUser.current()
+                var pronoun1 = ""
+                var pronoun2 = ""
+                var pronoun3 = ""
+                if user?.attractionPreference == "He/Him" {
+                    pronoun1 = "He"
+                    pronoun2 = "Him"
+                    pronoun3 = "Men"
+                } else if user?.attractionPreference == "She/Hers" {
+                    pronoun1 = "She"
+                    pronoun2 = "Her"
+                    pronoun3 = "Women"
+                } else {
+                    pronoun1 = "They"
+                    pronoun2 = "Them"
+                    pronoun3 = "People"
+                }
 
-        if (rizzult == nil) {
-            damnBadLabel.text = "No Data"
-            wantMeFrLabel.text = "No Data"
-            goingToTalkLabel.text = "No Data"
-            numberTalkedToLabel.text = "No Data"
-            commsGottenLabel.text = "No Data"
-            notesLabel.text = "No Data"
-            emojiRating.text = "No Data"
-            textRating.text = "No Data"
-        } else {
-            damnBadLabel.text = "\(rizzult?.badsQuantity ?? -1)x Damn \(pronoun1.lowercased()) bad"
-            wantMeFrLabel.text = "\(rizzult?.wantMeFrFRQuantity ?? -1)x \(pronoun1) want me frfr"
-            goingToTalkLabel.text = "\(rizzult?.goingToTalkToQuantity ?? -1)x I'm going to talk to \(pronoun2.lowercased())"
-            numberTalkedToLabel.text = "\(rizzult?.howManyTalkedTo ?? -1)x \(pronoun3) talked to"
-            commsGottenLabel.text = "\(rizzult?.numberComunications ?? -1)x Numbers gotten"
-            notesLabel.text = "\(rizzult?.descriptionOfSituation ?? "No Data")"
-            
-            
-            let check  = rizzult?.ownerRizz ?? -1
-            
-            if case -1...50 = check {
-                emojiRating.textColor = UIColor.red
-                emojiRating.text = "🤣 L Rizz"
-                textRating.text = "But it's okay!"
-            } else if case 51...70 = check {
-                emojiRating.textColor = UIColor.orange
-                emojiRating.text = "😕 Mid Rizz"
-                textRating.text = "You can do better!"
-            } else if case 71...80 = check {
-                emojiRating.textColor = UIColor(red: 0/255, green: 204/255, blue: 0/255, alpha: 1)
-                emojiRating.text = "😏 Valid Rizz"
-                textRating.text = "Almost a W!"
-            } else if case 81...100 = check {
-                emojiRating.textColor = UIColor(red: 0/255, green: 204/255, blue: 0/255, alpha: 1)
-                emojiRating.text = "🙌 W Rizz"
-                textRating.text = "Congrats!"
+                if (rizzult == nil) {
+                    damnBadLabel.text = "No Data"
+                    wantMeFrLabel.text = "No Data"
+                    goingToTalkLabel.text = "No Data"
+                    numberTalkedToLabel.text = "No Data"
+                    commsGottenLabel.text = "No Data"
+                    notesLabel.text = "No Data"
+                    emojiRating.text = "No Data"
+                    textRating.text = "No Data"
+                } else {
+                    damnBadLabel.text = "\(rizzult?.badsQuantity ?? -1)x Damn \(pronoun1.lowercased()) bad"
+                    wantMeFrLabel.text = "\(rizzult?.wantMeFrFRQuantity ?? -1)x \(pronoun1) want me frfr"
+                    goingToTalkLabel.text = "\(rizzult?.goingToTalkToQuantity ?? -1)x I'm going to talk to \(pronoun2.lowercased())"
+                    numberTalkedToLabel.text = "\(rizzult?.howManyTalkedTo ?? -1)x \(pronoun3) talked to"
+                    commsGottenLabel.text = "\(rizzult?.numberComunications ?? -1)x Numbers gotten"
+                    notesLabel.text = "\(rizzult?.descriptionOfSituation ?? "No Data")"
+                    
+                    
+                    let check  = rizzult?.ownerRizz ?? -1
+                    
+                    if case -1...50 = check {
+                        emojiRating.textColor = UIColor.red
+                        emojiRating.text = "🤣 L Rizz"
+                        textRating.text = "But it's okay!"
+                    } else if case 51...70 = check {
+                        emojiRating.textColor = UIColor.orange
+                        emojiRating.text = "😕 Mid Rizz"
+                        textRating.text = "You can do better!"
+                    } else if case 71...80 = check {
+                        emojiRating.textColor = UIColor(red: 0/255, green: 204/255, blue: 0/255, alpha: 1)
+                        emojiRating.text = "😏 Valid Rizz"
+                        textRating.text = "Almost a W!"
+                    } else if case 81...100 = check {
+                        emojiRating.textColor = UIColor(red: 0/255, green: 204/255, blue: 0/255, alpha: 1)
+                        emojiRating.text = "🙌 W Rizz"
+                        textRating.text = "Congrats!"
+                    }
+                }
+            } catch let error {
+                print(error)
             }
         }
+        
+        
     }
 }
